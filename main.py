25367c48ac2825f4b5bc22321dedc45c8bac65d4
Mikey <shpoopdy@gmail.com>
2021-04-10 14:43:13 -0700

    Created new route and user.html.

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", user_name=name)
+
 
 if __name__ == "__main__":
     app.run()
