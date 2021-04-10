89d0b90073051a69e25fd12cca1eaccd6979541c
Mikey <shpoopdy@gmail.com>
2021-04-10 15:18:45 -0700

    Created error pages.

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ def user(name):
     return render_template("user.html", user_name=name)
 
 
+# Error Page
+# Invalide URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run()
